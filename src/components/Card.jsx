68e8a15b524c97ed397f9b86c9d057cfa04049d0
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-09-25 11:48:36 -0300

    coração

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ export const Card = ({props}) => {
   return(
       <div className={style.box}>
         <div className={style.container}>
+          <img src="../assets/img/naipe-heart-as.png" alt="" className={style.topleftimage} />
           <form method='post' className={style.form}>
             <label htmlFor="#name">Insira seu nome</label>
             <input id='name' name='name' type="text"/>
